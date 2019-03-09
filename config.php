a84fb9fe326af31ba5d3527c03a432832ca84c51
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-09 09:17:19 +0300

    config.php is added

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$result = [
+    'dbDriver'   => 'mysql',
+    'dbHost'     => 'php-2hw02.mac',
+    'dbPort'     => '8889',
+    'dbName'     => 'php2hw02',
+    'dbUserName' => 'eug',
+    'dbPassword' => '123',
+];
+
+return $result;
