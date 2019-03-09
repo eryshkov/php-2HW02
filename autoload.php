7bd91641ea59190777daee896c3e8a0739c59382
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-09 09:08:22 +0300

    autoload is fixed

--- autoload.php
+++ autoload.php
@@ -1,7 +1,9 @@
 <?php
 
-function __autoload($class)
+function autoload($class)
 {
     $file = str_replace('\\', '/', $class) . '.php';
     require __DIR__ . '/' . $file;
 }
+
+spl_autoload_register('autoload');
